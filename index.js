d1e9ff794bf3db54ba618c8d83703b5b64b6400e
Saswata Alex Majumder <majumder.ua@gmail.com>
2023-09-10 23:54:28 -0400

    dom modifying

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const elem = document.getElementById("main");
+elem.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Coco Gauff is the champion!";
+document.body.append(newHeader);
